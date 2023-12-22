09423900994a09c6e4c27c2bbaf1c7a7453a26d4
Matheus Mendes Farias <92549077+MthsFarias@users.noreply.github.com>
2023-12-22 11:16:08 -0300

    Add files via upload

---
+++ calculo_area.php
@@ -0,0 +1,18 @@
+﻿<html lang="pt-br"> 
+  <head>
+  <!--Calcule a área de um quadrado,retangulo e uma circuferência! -->
+  </head>
+  <body>
+    <h1> CALCULO  </h1>
+	<?php
+	  $calculo_quadrado = $_POST["lateral"];
+	  $calculo_quadrado = pow($calculo_quadrado,2);
+	  
+	  $raio = $_POST["raio"];
+	  $circuferência = ((2*pi()*$raio);
+	  
+	  echo "<p> Calculo da área do quadrado: $calculo_quadrado  </p>";
+	  echo "<p> Calculo da cincunferência: $circuferência </p>";
+	?>
+  </body>
+  </html>
---
+++ index.php
@@ -0,0 +1,19 @@
+﻿<html lang="pt-br"> 
+  <head>
+  <!--Calcule a área de um quadrado,retangulo e uma circuferência! -->
+  </head>
+  <body>
+    <h1> CALCULO  </h1>
+	<form action="calculo_area.php" method="post" > 
+	 <p>
+	 Valor da Lateral do quadrado: <input type="text" name="lateral" > 
+	 </p>
+	 <p>
+	 Valor do Raio da cincunferência: <input type="text" name="raio">
+	 </p>
+	 <p>
+	   <input type="submit" value="calcular">
+	 </p>
+	</form>
+  </body>
+  </html>
