490127e0021c8ab18056a8c4460f07bbb6c802fe
Matheus Mendes Farias <92549077+MthsFarias@users.noreply.github.com>
2023-12-28 18:28:57 +0000

    Alterando o operador pow e atualizando nomenclatura

--- calculo_area.php
+++ calculo_area.php
@@ -6,13 +6,12 @@
     <h1> CALCULO  </h1>
 	<?php
 	  $calculo_quadrado = $_POST["lateral"];
-	  $calculo_quadrado = pow($calculo_quadrado,2);
-	  
+	  $calculo_quadrado = ($calculo_quadrado**2); //Alterando o operador pow
 	  $raio = $_POST["raio"];
-	  $circuferência = ((2*pi()*$raio);
+	  $circuferencia = (2*pi()*$raio);
 	  
 	  echo "<p> Calculo da área do quadrado: $calculo_quadrado  </p>";
-	  echo "<p> Calculo da cincunferência: $circuferência </p>";
+	  echo "<p> Calculo da cincunferência: $circuferencia </p>";
 	?>
   </body>
   </html>
